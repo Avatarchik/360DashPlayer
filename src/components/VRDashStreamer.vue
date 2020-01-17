447606603f9d95926ba43c4812706b3ff591b028
Christian Bach <christianbach1689@googlemail.com>
2020-01-17 16:10:03 +0100

    addon

---
+++ src/components/VRDashStreamer.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-scene embedded>
+            <a-assets>
+              <video id="dashjs" data-dashjs-player src="http://localhost/360DashPlayer/media/2019_Oberwiesenthal/h264/oberwiesenthal.mpd" class="player" controls autoplay webkit-playsinline></video>
+            </a-assets>
+            <a-videosphere id="videosphere" src="#dashjs"></a-videosphere>
+          </a-scene>
+ </template>
+
+<script>
+import 'aframe'
+export default {
+  name: "VRDashStreamer",
+  mounted: function () {
+      
+      var playerElement = document.getElementById("dashjs");
+      playerElement.play();
+  }
+
+};
+</script>
